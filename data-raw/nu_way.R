4b1154528a13c50201728f7c10c1ef78d3572b90
beanumber <ben.baumer@gmail.com>
2020-12-09 17:56:54 -0500

    updated web page

--- data-raw/nu_way.R
+++ data-raw/nu_way.R
@@ -6,7 +6,7 @@ library(tidyverse)
 
 library(sf)
 
-lots <- read_sf(here::here("data-raw/lots_20190816/"))
+# lots <- read_sf(here::here("data-raw/lots_20190816/"))
 
 nu_way <- read_delim("data-raw/nu_way.tsv", delim = "\t") %>%
   mutate(date = lubridate::mdy(`Rec. Date`)) %>%
@@ -20,21 +20,33 @@ properties <- nu_way %>%
     end = max(date)
   )
 
+library(sf)
+library(tidygeocoder)
+
 nu_way_zillow <- read_csv("data-raw/nu_way_zillow.csv")
 
+nu_way_sf <- properties %>%
+  filter(str_detect(address, "^[0-9]")) %>%
+  filter(!str_detect(address, "0 ROUND HILL RD")) %>%
+  mutate(lkup = paste(address, "Northampton, MA 01060")) %>%
+  geocode(lkup, method = "osm") %>%
+  st_as_sf(coords = c("long", "lat")) %>%
+  st_set_crs(4326)
 
 
-handzel <- lots %>%
-  inner_join(nu_way, by = c("OwnAdd" = "address")) %>%
-  st_transform(4326)
+
+
+#handzel <- lots %>%
+#  inner_join(nu_way, by = c("OwnAdd" = "address")) %>%
+#  st_transform(4326)
 
 
 library(leaflet)
 leaflet() %>%
   addTiles() %>% 
   addProviderTiles(providers$Esri.WorldImagery) %>%
-  addPolygons(data = nu_way_zillow, popup = ~OwnAdd)
+  addMarkers(data = nu_way_sf, popup = ~address)
   
 
 
-usethis::use_data(DATASET, overwrite = TRUE)
+usethis::use_data(nu_way_sf, overwrite = TRUE)
